875b2efea267841b18e0c08e95fe5d1a16194dfd
adekunleaishat1 <aishatadekunle877@gmail.com>
2025-01-21 10:32:31 +0100

    mongodb signup

--- index.js
+++ index.js
@@ -9,15 +9,21 @@ app.use(express.urlencoded({extended:true}))
 
 
 const userschema = mongoose.Schema({
-   username:{type:String},
-   email:{type:String},
-   password:{type:String}
+   username:{type:String,trim:true, required:true},
+   email:{type:String,trim:true, unique:true, required:true},
+   password:{type:String,trim:true, required:true}
+})
+const usermodel = mongoose.model("user_collections", userschema )
+
+const todoschema = mongoose.Schema({
+  title:{type:String,trim:true, required:true},
+  content:{type:String, trim:true, required:true}
 })
 
- const usermodel = mongoose.model("User_collection", userschema )
+const todomodel = mongoose.model("todo_collection", todoschema)
 
 let userarray = []
-let todoarray = []
+
 let message;
 app.get("/",(request, response)=>{
 //   response.send("Welcome to your Node class")
@@ -40,23 +46,30 @@ app.get("/user", (req, res)=>{
 })
 
 app.get("/login",(req,res)=>{
-  res.render("login")
+  res.render("login",{message})
 })
 
-app.get("/todo",(req, res)=>{
+app.get("/todo",async(req, res)=>{
+ const todoarray = await todomodel.find()
+ console.log(todoarray);
+ 
   res.render("todo",{name:"Tola",todoarray})
 })
 
 
-app.post("/todo",(req, res)=>{
+app.post("/todo", async(req, res)=>{
    console.log(req.body);
    const {title, content} = req.body
    if (!title || !content) {
        message = "Input fields are mandatory"
        res.redirect("/todo")
    }else{
-       todoarray.push(req.body)
-       res.redirect("/todo")
+      //  todoarray.push(req.body)
+    const todo =  await todomodel.create(req.body)
+    if (todo) {
+
+      res.redirect("/todo")
+    }
    }
 })
 
@@ -84,18 +97,35 @@ app.post("/user/signup", async(req, res)=>{
   }
 })
 
-app.post("/user/login",(req,res)=>{
-  console.log(req.body);
-  const {email, password } = req.body
- const existuser =  userarray.find((user)=> user.email === email && user.password === password)
- console.log(existuser);
- if (existuser) {
-  console.log("login successful");
-  res.redirect("/")
- }else{
-  console.log("login failed");
-  res.redirect("/login")
- }
+app.post("/user/login",async(req,res)=>{
+  try {
+    console.log(req.body);
+    const {email, password } = req.body
+   const existuser = await usermodel.findOne({email})
+   console.log(existuser);
+   if (existuser && existuser.password === password) {
+    console.log("Login successful");
+    message = "Login succesful"
+    res.redirect("/")
+   }else{
+     message = "Invalid user"
+      res.redirect("/login")
+   }
+   
+  } catch (error) {
+    console.log(error);
+    
+  }
+
+//  const existuser =  userarray.find((user)=> user.email === email && user.password === password)
+//  console.log(existuser);
+//  if (existuser) {
+//   console.log("login successful");
+//   res.redirect("/")
+//  }else{
+//   console.log("login failed");
+//   res.redirect("/login")
+//  }
 })
 
 app.get("/edit/todo/:index", (req,res)=>{
